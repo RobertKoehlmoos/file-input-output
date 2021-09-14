a2a27fbb498493ad66b95cfb74bf955ba863f7ba
rob <robert.koehlmoos@gmail.com>
2021-09-14 00:34:34 -0400

    Not done with setup yet

---
+++ problems.py
@@ -0,0 +1,48 @@
+"""
+In the sample text folder you will find a file named "nums.json". It is a list of numbers stored in JSON format.
+Create a new file named "nums_squares_map.json" in the sample text folder that contains a dictionary of all the numbers
+in the first json as keys mapped to their values squared.
+Don't worry about the json module automatically converting integers to strings when you store it.
+"""
+
+
+def convert_json():
+    pass
+
+
+"""
+In the sample_text folder you will find a file named "sonnets.txt". This file contains (almost) all the
+sonnets written by William Shakespeare in a numbered order.
+
+Create a new folder named "sonnets", and store each sonnet in it as a separate file named "sonnet_x.txt" where the x
+is replaced with the sonnet number. The contents of each file should be only the lines of text for each sonnet, so
+you don't need the empty lines above or below. Don't forget to have a newline at the end of the last line (that
+should make things easier rather than harder)
+"""
+
+
+def separate_sonnets():
+    pass
+
+
+"""
+In the sample_text folder you will find a file named "iso_8859-1.txt". This file contains a list of the
+hexadecimal representation of commonly used characters.
+
+The hex values and descriptions are currently in two columns.
+Write a function that converts this file to have all hex values and description in a single column,
+shifting the second column to continue immediately under the first.
+
+Then write a function that converts it back to how it was.
+
+Yes, you could just store a string copy of the entire file you want to write in your function. You are very clever. Don't do that.
+You probably want to save a safe copy of the original on the indescribably small chance you mess it up along the way.
+"""
+
+
+def two_columns_to_one():
+    pass
+
+
+def one_column_to_two():
+    pass
---
+++ testing_text/README.txt
@@ -0,0 +1,1 @@
+These files are for testing your solutions. Don't try and copy them to solve your problems.
