00b15f9868525f100540f32083d0ef5dfe0573c3
rob <robert.koehlmoos@gmail.com>
2021-09-14 22:40:28 -0400

    Fixed problems due to windows vs linux slashes in file paths

--- problems_test.py
+++ problems_test.py
@@ -2,21 +2,22 @@ from problems import convert_json, separate_sonnets, two_columns_to_one
 import shutil
 import json
 import os
+from pathlib import Path
 
 
 def test_convert_json():
-    if os.path.isfile("sample_text\\string_lengths_map.json"):
-        os.remove("sample_text\\string_lengths_map.json")
+    if os.path.isfile(Path("sample_text/string_lengths_map.json")):
+        os.remove(Path("sample_text/string_lengths_map.json"))
 
     convert_json()
 
-    with open("sample_text\\string_lengths_map.json", "r") as f:
+    with open(Path("sample_text/string_lengths_map.json"), "r") as f:
         provided = json.load(f)
-    with open("testing_text\\string_lengths.json", "r") as f:
+    with open(Path("testing_text/string_lengths.json"), "r") as f:
         expected = json.load(f)
 
-    if os.path.isfile("sample_text\\string_lengths_map.json"):
-        os.remove("sample_text\\string_lengths_map.json")  # cleaning up after ourselves
+    if os.path.isfile(Path("sample_text/string_lengths_map.json")):
+        os.remove(Path("sample_text/string_lengths_map.json"))  # cleaning up after ourselves
 
     assert provided == expected
 
@@ -26,8 +27,9 @@ def test_separate_sonnets():
         shutil.rmtree("sonnets")  # removing any preexisting files
     separate_sonnets()
 
-    for file_path in os.listdir("testing_text\\sonnets"):
-        with open(f"sonnets\\{file_path}", "r") as provided, open(f"testing_text\\sonnets\\{file_path}") as expected:
+    for file_path in os.listdir(Path("testing_text/sonnets")):
+        with open(Path(f"sonnets/{file_path}"), "r") as provided,\
+             open(Path(f"testing_text/sonnets/{file_path}")) as expected:
             assert provided.read() == expected.read()
 
     if os.path.isdir("sonnets"):
@@ -35,15 +37,15 @@ def test_separate_sonnets():
 
 
 def test_two_to_one_columns():
-    if os.path.isfile("sample_text\\iso_8859-1_two_columns.txt"):
-        os.remove("sample_text\\iso_8859-1_two_columns.txt")
+    if os.path.isfile(Path("sample_text/iso_8859-1_two_columns.txt")):
+        os.remove(Path("sample_text/iso_8859-1_two_columns.txt"))
 
     two_columns_to_one()
 
-    with open("sample_text\\iso_8859-1_two_columns.txt", "r") as provided, \
-            open("testing_text\\iso_8859-1_two_columns.txt") as expected:
+    with open(Path("sample_text/iso_8859-1_two_columns.txt"), "r") as provided, \
+            open(Path("testing_text/iso_8859-1_two_columns.txt")) as expected:
         for provided_line, expected_line in zip(provided, expected):
             assert provided_line.strip() == expected_line.strip()
 
-    if os.path.isfile("sample_text\\iso_8859-1_two_columns.txt"):
-        os.remove("sample_text\\iso_8859-1_two_columns.txt")
+    if os.path.isfile(Path("sample_text/iso_8859-1_two_columns.txt")):
+        os.remove(Path("sample_text/iso_8859-1_two_columns.txt"))
